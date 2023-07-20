05298f3437008ff72a803267b03f7f9093141664
Boulanger11 <leo.tcheco@gmail.com>
2023-07-19 21:16:14 -0300

    template-aula

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,22 +2,19 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { irParaCadastro } from '../../Routes/coordinator'
 import { ContainerForm, ContainerLogin, Input } from './styled'
+import useForm from '../../hooks/useForm'
 
 export default function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const navigate = useNavigate()
 
-  const onChangeEmail = (e) => {
-    setEmail(e.target.value)
-  }
-  const onChangeSenha = (e) => {
-    setPassword(e.target.value)
-  }
+  const {form, onChangeInputs, cleanFields} = useForm({email:'',password:''})
+
+  const navigate = useNavigate()
 
   const enviaLogin = (e) => {
     e.preventDefault()
-    console.log({ email, password })
+    console.log(form)
+
+    cleanFields()
   }
 
   return (
@@ -25,17 +22,25 @@ export default function Login() {
       <ContainerForm onSubmit={enviaLogin}>
         <label htmlFor='email'>Email:</label>
         <Input
+          name="email"
           id='email'
-          value={email}
-          onChange={onChangeEmail}
+          value={form.email}
+          onChange={onChangeInputs}
           placeholder="nome@email.com"
+          type='email'
+          required
         />
         <label htmlFor='senha'>Senha:</label>
         <Input
+          name="password"
           id='senha'
-          value={password}
-          onChange={onChangeSenha}
+          value={form.password}
+          onChange={onChangeInputs}
           placeholder="Digite sua senha"
+          type='password'
+          pattern='^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$'
+          title='Deve conter ao menos uma letra minúscula, uma letra maiúscula, um caractere espercial e ao menos 8 dos caracteres mencionados'
+          required
         />
         <button>Fazer Login</button>
       </ContainerForm>
